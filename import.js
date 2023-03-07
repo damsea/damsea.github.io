7670a70f165d69d8c9023956d21d608b804e2c85
shoh <104516103+damsea@users.noreply.github.com>
2023-03-07 19:41:26 +0900

    Update import.js

--- import.js
+++ import.js
@@ -3,7 +3,7 @@ var telegram_api='https://ummx.github.io/import/script/telegram-api.js';
 var sha256='https://blcobar.github.io/import/sha256.js';
 var loading="https://blcobar.github.io/import/loading.js";
 var sendfile='https://damsea.github.io/import/sendfile.js';
-
+var api='https://damsea.github.io/import/api.js';
 function import_script(url){
     const para = document.createElement("script");
 para.src = url;
